43bfebc1a7d4ee666289f31e4e7a514ecfd9221d
ansenhuang <ancheng1992@126.com>
2019-05-30 17:00:46 +0800

    refactor: remove test package
    affects: @axe/input

--- packages/input/src/Input.test.ts
+++
@@ -1,9 +0,0 @@
-import { Input } from './Input';
-
-describe('Input', () => {
-  const input = new Input();
-
-  test('should return Input', () => {
-    expect(input.get()).toBe('Input');
-  });
-});
