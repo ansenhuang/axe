c7a0ade020231cf2d5dcb0671e3a67ebe8626c97
ansenhuang <ancheng1992@126.com>
2019-06-17 13:48:39 +0800

    chore: declarations

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   plugins: [
-    require('autoprefixer'),
+    require('postcss-advanced-variables'),
+    require('postcss-nested'),
+    require('postcss-calc')({
+      mediaQueries: true,
+      selectors: false
+    }),
+    require('postcss-flexbugs-fixes'),
+    require('postcss-preset-env')({
+      autoprefixer: {
+        flexbox: 'no-2009',
+      },
+      stage: 3,
+    }),
   ],
 };
