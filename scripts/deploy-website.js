86f13591f82977fa742ccb0ca0f216b346e6e434
ansenhuang <ancheng1992@126.com>
2019-06-18 18:35:49 +0800

    chore: deploy force

--- scripts/deploy-website.js
+++ scripts/deploy-website.js
@@ -6,6 +6,6 @@ spawnSync('touch', ['.nojekyll'], { stdio: 'ignore', cwd: websitePath });
 spawnSync('git', ['init'], { stdio: 'ignore', cwd: websitePath });
 spawnSync('git', ['add', '.'], { stdio: 'ignore', cwd: websitePath });
 spawnSync('git', ['commit', '-m', 'deploy: update gh-pages'], { stdio: 'ignore', cwd: websitePath });
-spawnSync('git', ['push', 'git@github.com:ansenhuang/axe.git', 'master:gh-pages'], { stdio: 'ignore', cwd: websitePath });
+spawnSync('git', ['push', '-f', 'git@github.com:ansenhuang/axe.git', 'master:gh-pages'], { stdio: 'inherit', cwd: websitePath });
 
-console.log('Website deployed!!!');
+console.log('\nWebsite deployed!!!');
