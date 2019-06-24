f29c3f79b385c5514e44c57b48e53d1416d7a170
ansenhuang <ancheng1992@126.com>
2019-06-24 14:57:41 +0800

    test: tips

--- tests/tips.test.ts
+++ tests/tips.test.ts
@@ -1,7 +1,27 @@
 import tips from '@axe/tips';
 
-describe('Please add your own test!', () => {
-  test('test', () => {
-    expect(typeof tips.show).toBe('function');
+describe('initial instance of tips', () => {
+  const tipsEl = <HTMLElement>document.body.querySelector('.tips');
+
+  test('tips should inject to body', () => {
+    expect(tipsEl).toBeInstanceOf(HTMLDivElement);
+  });
+
+  test('tips should be hidden', () => {
+    expect(tipsEl.style.display).toBe('none');
+  });
+
+  test('show tips', () => {
+    const callback = jest.fn();
+    const content = 'Hello Tips!';
+    const duration = 2000;
+    tips.show({ content, duration }, callback);
+
+    expect(tipsEl.style.display).not.toBe('none');
+    expect((<HTMLElement>tipsEl.querySelector('.body')).textContent).toBe(content);
+    setTimeout(() => {
+      expect(callback).toBeCalled();
+      expect(tipsEl.style.display).toBe('none');
+    }, duration + 1);
   });
 });
