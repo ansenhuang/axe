37bea058d6e27a5297e0c68955b2d7560fe5f891
ansenhuang <ancheng1992@126.com>
2019-06-26 10:18:20 +0800

    style: translate example to English

--- examples/precision/index.ts
+++ examples/precision/index.ts
@@ -1,30 +1,21 @@
-import {
-  divide,
-  float2Int,
-  getDecimalLength,
-  minus,
-  plus,
-  times,
-} from '@axe/precision';
+import { divide, float2Int, getDecimalLength, minus, plus, times } from '@axe/precision';
 
 (document.getElementById('root') as Element).innerHTML = [
-  '<h1>精确计算示例</h1>',
+  '<h1>precision example</h1>',
 
-  '<h2>原始计算</h2>',
-  `<p>加法：0.1 + 0.2 = ${0.1 + 0.2}</p>`,
-  `<p>减法：0.3 - 0.2 = ${0.3 - 0.2}</p>`,
-  `<p>乘法：0.1 * 0.2 = ${0.1 * 0.2}</p>`,
-  `<p>除法：0.02 / 0.1 = ${0.02 / 0.1}</p>`,
+  '<h2>before use precision</h2>',
+  `<p>plus(+)   : 0.1 + 0.2 = ${0.1 + 0.2}</p>`,
+  `<p>minus(-)  : 0.3 - 0.2 = ${0.3 - 0.2}</p>`,
+  `<p>times(*)  : 0.1 * 0.2 = ${0.1 * 0.2}</p>`,
+  `<p>divide(/) : 0.02 / 0.1 = ${0.02 / 0.1}</p>`,
 
-  '<h2>精确计算</h2>',
-  `<p>加法：0.1 + 0.2 = ${plus(0.1, 0.2)}</p>`,
-  `<p>减法：0.3 - 0.2 = ${minus(0.3, 0.2)}</p>`,
-  `<p>乘法：0.1 * 0.2 = ${times(0.1, 0.2)}</p>`,
-  `<p>除法：0.02 / 0.1 = ${divide(0.02, 0.1)}</p>`,
+  '<h2>after use precision</h2>',
+  `<p>plus(+)   : 0.1 + 0.2 = ${plus(0.1, 0.2)}</p>`,
+  `<p>minus(-)  : 0.3 - 0.2 = ${minus(0.3, 0.2)}</p>`,
+  `<p>times(*)  : 0.1 * 0.2 = ${times(0.1, 0.2)}</p>`,
+  `<p>divide(/) : 0.02 / 0.1 = ${divide(0.02, 0.1)}</p>`,
 
-  '<h2>计算工具</h2>',
-  `<p>获取小数长度：getDecimalLength(2.000005) = ${getDecimalLength(
-    2.000005,
-  )}</p>`,
-  `<p>小数转化为整数：float2Int(2.100005) = ${float2Int(2.100005)}</p>`,
+  '<h2>precision tools</h2>',
+  `<p>getDecimalLength(2.000005) = ${getDecimalLength(2.000005)}</p>`,
+  `<p>float2Int(2.100005) = ${float2Int(2.100005)}</p>`,
 ].join('');
